5a01cc6ae0aa6be481ff0ff620ab4432ad894bec
Matt Sell <mwsell@gmail.com>
2018-06-29 23:13:21 -0500

    formik wired up

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,11 +1,19 @@
 import * as React from "react";
 import { RegisterView } from "./view/RegisterView";
 
-export const RegisterConnector = () => <RegisterView />
-
 // typical react web
 // container -> view
 
 // because we want to create web and mobile we want
 // container -> connector -> view
 // controller -> connector -> view
+
+export class RegisterConnector extends React.PureComponent {
+  dummySubmit = async (values: any) => {
+    console.log(values);
+    return null;
+  };
+  render() {
+    return <RegisterView submit={this.dummySubmit} />;
+  }
+}
--- packages/web/src/modules/register/view/RegisterView.tsx
+++ packages/web/src/modules/register/view/RegisterView.tsx
@@ -1,10 +1,73 @@
-import * as React from 'react'
-import Button from 'antd/lib/button'
+import * as React from "react";
+import { withFormik, FormikErrors, FormikProps } from "formik";
+import { Form, Icon, Input, Button } from "antd";
+const FormItem = Form.Item;
 
-export class RegisterView extends React.PureComponent {
+interface FormValues {
+  password: string;
+  email: string;
+}
+
+interface Props {
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
+}
+
+export class C extends React.PureComponent<FormikProps<FormValues> & Props> {
   render() {
+      const { handleChange, handleBlur, handleSubmit, values } = this.props;
     return (
-      <Button type="primary">Register</Button>
-    )
+      <form style={{ display: "flex" }} onSubmit={handleSubmit}>
+        <div style={{ width: 400, margin: "auto" }} className="login-form">
+          <FormItem>
+            <Input            
+              name="email"
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Email"
+              onChange={handleChange}
+              value={values.email}
+              onBlur={handleBlur}
+            />
+          </FormItem>
+          <FormItem>
+            <Input
+              name="password"
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+              value={values.password}
+              onBlur={handleBlur}
+            />
+          </FormItem>
+          <FormItem>
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+               
+            >
+              Register
+            </Button>
+            Or <a href="">Login</a>
+          </FormItem>
+        </div>
+      </form>
+    );
   }
 }
+
+export const RegisterView = withFormik<Props, FormValues>({
+  mapPropsToValues: () => ({
+    email: "",
+    password: ""
+  }),
+  handleSubmit: async (values, { props, setErrors, setSubmitting }) => {
+    const errors = await props.submit(values);
+    if(errors) {
+        setErrors(errors)
+    }
+  }
+})(C);
